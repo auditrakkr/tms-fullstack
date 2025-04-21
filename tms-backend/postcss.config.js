aca95d433bb5a59d771fd6034863ba4f94ad7ced
Sochukwuma Nwokoye <somanwokoye@gmail.com>
2025-04-21 20:29:07 +0100

    Revert "Functionalities/tailwind"

--- tms-backend/postcss.config.js
+++ tms-backend/postcss.config.js
@@ -2,10 +2,6 @@ module.exports = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
-    ...(process.env.NODE_ENV === 'production' ? [
-      require('cssnano')({
-        preset: 'default',
-      }),
-    ] : []),
-  ],
+    ...(process.env.NODE_ENV === 'production' ? [require('cssnano')] : [])
+  ]
 }
